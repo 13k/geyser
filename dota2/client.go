8f5287db35ea34f9121c00bee661c540dfba7111
Kiyoshi '13k' Murata <kbmurata@gmail.com>
2019-12-13 17:04:44 -0300

    Add base dota2 sub-package

---
+++ dota2/client.go
@@ -0,0 +1,33 @@
+/*
+Package dota2 implements an HTTP client for the (undocumented) Dota 2 Web API.
+
+For more information, refer to the main "geyser" package documentation.
+*/
+package dota2
+
+import (
+	"github.com/13k/geyser"
+)
+
+const (
+	HostURL = "https://www.dota2.com/webapi"
+)
+
+type Client struct {
+	*geyser.Client
+}
+
+// New creates a new Dota2 API client.
+func New(options ...geyser.ClientOption) (*Client, error) {
+	options = append(options, geyser.WithBaseURL(HostURL))
+
+	baseClient, err := geyser.New(options...)
+
+	if err != nil {
+		return nil, err
+	}
+
+	client := &Client{baseClient}
+
+	return client, nil
+}
