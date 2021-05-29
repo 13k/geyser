895bd63ed5b955cfdfc18f77140ad5ef9e7a570d
Kiyoshi '13k' Murata <kbmurata@gmail.com>
2021-05-29 13:38:55 -0300

    Use `steamid.SteamID` for steam IDs instead of uint64

--- steam/isteam_user_auth_helpers.go
+++ steam/isteam_user_auth_helpers.go
@@ -4,16 +4,16 @@ import (
 	"crypto/aes"
 	"crypto/rand"
 	"net/url"
-	"strconv"
 
 	"github.com/13k/go-steam"
 	"github.com/13k/go-steam-resources/steamlang"
 	"github.com/13k/go-steam/cryptoutil"
+	"github.com/13k/go-steam/steamid"
 )
 
 // ISteamUserAuthAuthenticateUserFormData generates request form data for method
 // ISteamUserAuth/AuthenticateUser.
-func ISteamUserAuthAuthenticateUserFormData(steamID uint64, loginKey string) (url.Values, error) {
+func ISteamUserAuthAuthenticateUserFormData(steamID steamid.SteamID, loginKey string) (url.Values, error) {
 	sessionKey := make([]byte, 32)
 
 	if _, err := rand.Read(sessionKey); err != nil {
@@ -40,9 +40,9 @@ func ISteamUserAuthAuthenticateUserFormData(steamID uint64, loginKey string) (ur
 
 	values := url.Values{}
 
-	values.Add("steamid", strconv.FormatUint(steamID, 10))
-	values.Add("sessionkey", string(encryptedSessionKey))
-	values.Add("encrypted_loginkey", string(encryptedLoginKey))
+	values.Set("steamid", steamID.FormatString())
+	values.Set("sessionkey", string(encryptedSessionKey))
+	values.Set("encrypted_loginkey", string(encryptedLoginKey))
 
 	return values, nil
 }
